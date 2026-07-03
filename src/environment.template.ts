4744d3911c52d95185ffafe353791e3747fc7460
Prateek-Pardeshi <free.host.55566@gmail.com>
2026-07-03 13:44:32 +0530

    secrects preserved

---
+++ src/environment.template.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "${FIREBASE_API_KEY}",
+        authDomain: "${FIREBASE_AUTH_DOMAIN}",
+        projectId: "${FIREBASE_PROJECT_ID}",
+        storageBucket: "${FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${FIREBASE_MESSAGING_SENDER}",
+        appId: "${FIREBASE_APP_ID}",
+        measurementId: "${FIREBASE_MEASUREMENT_ID}"
+    }
+}
--- src/environment.ts
+++ src/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebaseConfig: {
-        apiKey: "AIzaSyBrysYRH4T1M9TCsz0PxvEc-cPaRtt6xxc",
-        authDomain: "financeapp-8e4de.firebaseapp.com",
-        projectId: "financeapp-8e4de",
-        storageBucket: "financeapp-8e4de.firebasestorage.app",
-        messagingSenderId: "869028708938",
-        appId: "1:869028708938:web:a830a00b5b12a308f51cb3",
-        measurementId: "G-P835JRYTF3"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     }
 }
