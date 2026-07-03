8ace3909af1484fa1e74f26746b3ba638dd78712
Prateek-Pardeshi <free.host.55566@gmail.com>
2026-07-03 21:26:45 +0530

    secrets removed

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    googleApiKey: "AIzaSyBQJJCnKI_zl0j7dBAJFWnLQBQpwi1TrAc",
-    googleClientId: "372954632265-vgvgo3btb3jdpfleje71u99578ci5srm.apps.googleusercontent.com",
+    googleApiKey: "",
+    googleClientId: "",
     firebaseConfig: {
-        apiKey: "AIzaSyBrysYRH4T1M9TCsz0PxvEc-cPaRtt6xxc",
-        authDomain: "financeapp-8e4de.firebaseapp.com",
-        projectId: "financeapp-8e4de",
-        storageBucket: "financeapp-8e4de.firebasestorage.app",
-        messagingSenderId: "869028708938",
-        appId: "1:869028708938:web:a830a00b5b12a308f51cb3",
-        measurementId: "G-P835JRYTF3"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     }
 }
